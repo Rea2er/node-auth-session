c958f53547bec81b21d114701b2e654c23f8c04f
Ruize Nie <rz376775@dal.ca>
2020-08-06 00:31:36 -0300

    mongodb config set up and redis set up

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import Redis from "ioredis";
+import {
+  REDIS_OPTIONS,
+  SESSION_OPTIONS,
+  APP_PORT,
+  MONGO_URI,
+  MONGO_OPTIONS,
+} from "./config";
 
-const app = express();
+(async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
 
-app.get("/", (req, res) => res.json({ message: "OK" }));
+  const RedisStore = connectRedis(session);
+  const client = new Redis(REDIS_OPTIONS);
 
-app.listen(3000, () => console.log(`http://localhost:${3000}`));
+  const app = express();
+
+  app.use(session({ ...SESSION_OPTIONS, store: new RedisStore({ client }) }));
+
+  app.get("/", (req, res) => res.json({ message: "OK" }));
+
+  app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`));
+})();
