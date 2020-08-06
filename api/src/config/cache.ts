9b3aa7b2383eec5cb0bc125c282119db53146357
Ruize Nie <rz376775@dal.ca>
2020-08-06 01:40:57 -0300

    create mongodb and redis instance using docker container

--- api/src/config/cache.ts
+++ api/src/config/cache.ts
@@ -3,7 +3,7 @@ import { RedisOptions } from "ioredis";
 const {
   REDIS_PORT = 6379,
   REDIS_HOST = "localhost",
-  REDIS_PASSWORD = "password",
+  REDIS_PASSWORD = "secret",
 } = process.env;
 
 export const REDIS_OPTIONS: RedisOptions = {
--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from "mongoose";
 
 const {
   MONGO_USERNAME = "admin",
-  MONGO_PASSWORD = "password",
+  MONGO_PASSWORD = "secret",
   MONGO_HOST = "localhost",
   MONGO_PORT = 27017,
   MONGO_DATABASE = "auth",
@@ -11,6 +11,7 @@ const {
 export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${encodeURIComponent(
   MONGO_PASSWORD
 )}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+
 export const MONGO_OPTIONS: ConnectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
